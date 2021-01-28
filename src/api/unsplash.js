71b7316a90864c7ec35f2e7b68af7562d6f1558d
Hoang Huy <69996480+hoangcpt94@users.noreply.github.com>
2021-01-28 16:29:51 +0700

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -3,6 +3,6 @@ import axios from 'axios';
 export default axios.create({
 	baseURL: 'https://api.unsplash.com',
 	headers: {
-				Authorization: 'Client-ID QOpQVJRE10fQ3ep8Xz-aU7blFHpMGp5JruuvSqbisG8'
+		Authorization: 'Client-ID QOpQVJRE10fQ3ep8Xz-aU7blFHpMGp5JruuvSqbisG8'
 			}
 });
